0eb987f16551d45dcd84172ae167a735891e0a2a
Jaenk-99 <jaehy0429@gmail.com>
2022-07-01 02:34:55 +0900

    three-react 3

--- three-react/src/pages/one.js
+++ three-react/src/pages/one.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import {Canvas,useFrame} from '@react-three/fiber';
+import React, { useRef } from 'react';
+import { Canvas, useFrame } from "@react-three/fiber";
 
-export default function One(){
-  return(
+export default function One() {
+  function CoinMesh() {
+    const mesh = useRef(null);
+    useFrame(() => (mesh.current.rotation.y = mesh.current.rotation.z += 0.01)); // #2
+    return (
+      <mesh ref={mesh} scale={1.5}>
+        <cylinderBufferGeometry args={[1, 1, 0.3, 50]} />
+        <meshLambertMaterial attach="material" color="#ff9800" />
+      </mesh>
+    );
+  }
+
+  return (
     <>
       <Canvas>
-        
+        <pointLight position={[10, 10, 10]} />
+        <CoinMesh />
       </Canvas>
     </>
-  )
+  );
 }
--- three-react/src/pages/zero.js
+++ three-react/src/pages/zero.js
@@ -1,24 +1,16 @@
-import React, {useRef} from 'react';
-import {Canvas,useFrame} from '@react-three/fiber';
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 
-function CoinMesh() {
-    const mesh = useRef(null);
-    useFrame(() => (mesh.current.rotation.y =  mesh.current.rotation.z += 0.01)); // #2
-    return (
-      <mesh ref={mesh} scale={1.5}>
-        <cylinderBufferGeometry args={[1, 1, 0.3, 50]} />
-        <meshLambertMaterial attach="material" color="#ff9800" />
-      </mesh>
-    );
-  }
-
-export default function Zero(){
-  return(
+export default function Zero() {
+  return (
     <>
       <Canvas>
         <pointLight position={[10, 10, 10]} />
-        <CoinMesh/>
+        <mesh>
+          <sphereBufferGeometry />
+          <meshStandardMaterial color="hotpink" />
+        </mesh>
       </Canvas>
     </>
-  )
+  );
 }
